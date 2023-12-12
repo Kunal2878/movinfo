b054f780180599063f12493c12f11bb712a9fc51
Kunal <kunalpaul673@gmail.com>
2023-12-13 01:08:24 +0530

    commited

--- src/app/Anime.jsx
+++ src/app/Anime.jsx
@@ -242,6 +242,11 @@ setAnimeOverviewVisible(null);
     
     
       };
+      const closeSearch = () => {
+setAnimeOverviewVisible(null);
+    
+    
+      };
   return (
 
     <div className='m-3 w-screen'>
@@ -432,7 +437,7 @@ isAnimeOverviewVisible !==null && (
   <div className="w-10/12 lg:w-1/3 fixed  top-40 lg:top-20 left-10 flex flex-col  items-center justify-center lg:ml-40 bg-gray-900  mb-3 p-4 backdrop-filter backdrop-blur-md  rounded-md "  style={{zIndex:'200', maxHeight: '300px'}}>
 
  <div className="w-full flex flex-row justify-end">
-            <button className="btn btn-circle btn-outline" onClick={closeMovieOverview}>
+            <button className="btn btn-circle btn-outline" onClick={closeSearch}>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
